b82914d26b12dc7ea88bd3aa42aa2c76c2a44c17
noman081 <abdullahalnoman642@gmail.com>
2022-05-10 01:10:48 +0600

    After adding navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AboutUs from './Pages/AboutUs/AboutUs';
+import Home from './Pages/Home/Home';
+import NotFound from './Pages/NotFound/NotFound';
+import Header from './Pages/Shared/Header';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/about' element={<AboutUs />}></Route>
+
+        <Route path='*' element={<NotFound />}></Route>
+      </Routes>
     </div>
   );
 }
